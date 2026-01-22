bec6f5ae2cf2194deacae7cf21c8bad57d58317c
Rohan Girish <r.rohangirish@unibas.ch>
2026-01-22 15:26:17 +0100

    Handle empty input string gracefully

--- src/imcflibs/imagej/omerotools.py
+++ src/imcflibs/imagej/omerotools.py
@@ -57,11 +57,12 @@ def parse_url(client, omero_str):
     >>> for wrapper in img_wrappers:
     >>>    imp = wpr.toImagePlus(client)
     """
+    if omero_str is None or str(omero_str).strip() == "":
+        raise ValueError("No OMERO link or image ID provided.")
+
     # Sanitize the string
-    if omero_str is None:
-        return []
     omero_str = omero_str.strip()
-    
+
     image_ids = []
     dataset_ids = []
     image_wpr_list = []
